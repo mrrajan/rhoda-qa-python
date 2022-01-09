7abe51a127579422d7a2fc7b5b387e8d2f13e3e4
omveda <vedadashan@gmail.com>
2022-01-09 11:38:40 -0800

    Adding libraries

--- db_binding.py
+++ db_binding.py
@@ -1,8 +1,15 @@
 # Libraries
+
+import psycopg2
+import psycopg2.errorcodes
+import time
+import logging
+import random
+from pyservicebinding import binding
  
 # DB Binding Function 
 def db_bind(args_dict):
 #     arg = args_dict.get('arg1')
 #     predictor(arg)
-    return {'DB Binding': 'not implemented'}
+    return {'DB Binding': 'not implemented but reqs included'}
 
