8d0fb145cda2e53573c7048dac2d42d6c9e7301c
omveda <vedadashan@gmail.com>
2022-01-09 10:48:35 -0800

    adding files

---
+++ db_binding.py
@@ -0,0 +1,8 @@
+# Libraries
+ 
+# DB Binding Function 
+def db-bind(args_dict):
+#     arg = args_dict.get('arg1')
+#     predictor(arg)
+    return {'DB Binding': 'not implemented'}
+
---
+++ wsgi.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, jsonify, request
+from db_binding import db_bind
+
+application = Flask(__name__)
+
+
+@application.route('/')
+@application.route('/status')
+def status():
+    return jsonify({'status': 'ok'})
+
+
+@application.route('/dbbind', methods=['POST'])
+def create_db_bind():
+    data = request.data or '{}'
+    body = json.loads(data)
+    return jsonify(db_bind(body))
